3654d3f6f29adab416ef81144b8912c4dec51604
Gaming32 <soujournme@gmail.com>
2020-08-12 14:53:29 -0500

    finds first array

--- json2csv.js
+++ json2csv.js
@@ -1,3 +1,30 @@
+function locateArray(data) {
+    if (Array.isArray(data)) {
+        return data;
+    }
+    else {
+        for (var key in data) {
+            sub = data[key];
+            if (Array.isArray(sub)) {
+                return sub;
+            }
+            else {
+                var subArray = locateArray(sub);
+                if (subArray != null) {
+                    return subArray;
+                }
+            }
+        }
+    }
+    return null;
+}
+
 function json2csv(json) {
-    
+    return locateArray(json);
+}
+
+if (module) {
+    module.exports = {
+        json2csv: json2csv
+    };
 }
