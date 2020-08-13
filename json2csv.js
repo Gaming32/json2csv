0cb80eb2d42864515f891751135f258f631d377b
Gaming32 <soujournme@gmail.com>
2020-08-13 12:08:32 -0500

    converts simple json files

--- json2csv.js
+++ json2csv.js
@@ -5,22 +5,53 @@ function locateArray(data) {
     else {
         for (var key in data) {
             sub = data[key];
-            if (Array.isArray(sub)) {
-                return sub;
-            }
-            else {
-                var subArray = locateArray(sub);
-                if (subArray != null) {
-                    return subArray;
-                }
+            var subArray = locateArray(sub);
+            if (subArray != null) {
+                return subArray;
             }
         }
     }
     return null;
 }
 
+function generateFieldObject(array) {
+    var result = {};
+    for (var obj of array) {
+        for (var key in obj) {
+            if (!(key in result))
+                result[key] = [];
+            result[key].push(obj[key])
+        }
+    }
+    return result;
+}
+
+function csvFromFieldObject(fieldObject) {
+    if (Object.keys(fieldObject).length == 0)
+        return "";
+
+    var result = "";
+
+    for (var key in fieldObject) {
+        result += `"${key}",`;
+    }
+    result = result.substr(0, result.length - 1) + "\n";
+
+    var length = fieldObject[key].length;
+    for (i = 0; i < length; i++) {
+        for (var key in fieldObject) {
+            result += `"${fieldObject[key][i]}",`;
+        }
+        result = result.substr(0, result.length - 1) + "\n";
+    }
+
+    return result;
+}
+
 function json2csv(json) {
-    return locateArray(json);
+    var array = locateArray(json);
+    var fieldObject = generateFieldObject(array);
+    return csvFromFieldObject(fieldObject);
 }
 
 if (typeof module != "undefined") {
@@ -28,3 +59,12 @@ if (typeof module != "undefined") {
         json2csv: json2csv
     };
 }
+
+// console.log(csvFromFieldObject({
+//     id: [
+//         "1", "2"
+//     ],
+//     name: [
+//         "Matt Glosson", "Gmail Man"
+//     ]
+// }))
