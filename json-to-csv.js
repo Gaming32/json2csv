c8b681bd1139d8f5fcd9adca0035ea5ba2686d7b
theglossy1 <matt.glosson@gmail.com>
2020-08-13 13:34:54 -0500

    Update json-to-csv.js
    added argument checking

--- json-to-csv.js
+++ json-to-csv.js
@@ -1,5 +1,10 @@
 fs = require("fs");
 json2csv = require("./json2csv");
 
+if (process.argv[2]===undefined) {
+    console.log("Syntax: json-to-csv.js <input-filename>");
+    process.exit();
+}
+
 var argv = process.argv.slice(2);
 console.log(json2csv.json2csv(JSON.parse(fs.readFileSync(argv[0]))));
